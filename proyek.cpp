b2e2f07a4ea4e50b79570e5ad14965733c156784
Muhammad Reza Anggara <38530689+muhammadreza1044@users.noreply.github.com>
2018-04-30 22:54:34 +0700

    Update proyek.cpp

--- proyek.cpp
+++ proyek.cpp
@@ -1,22 +1,63 @@
 #include <iostream>
 using namespace std;
 
-struct node(){
+struct node{
   int data;
-  data *next;
+  node *next;
+};
+
+
+//node* tail;
+//struct Node(){
+ // int databaru;
+  //Node *head;
+//}
+node* head;
+
+void init(){
+      head = NULL;
+      //tail = NULL;
 }
 
-struct tnode(){
-  int databaru;
-  databaru *head;
+int cek(){
+  if(head==NULL) return 1;
+  else return 0;
 }
 
-void main(){
-  
-
-
+void barudepan(int databaru){
+  node *baru;
+baru = new node;
+baru->data = databaru;
+baru->next = baru;
+  if cek (head==1){
+    head=baru;
+    head->next=head;}
+  else {
+    help=head;
+    while (bantu->next!=head){
+      bantu=bantu->next;}
+    baru->next=head;
+    head=baru;
+    bantu->next=head;
+	}
+}
+void barubelakang(int databaru){
+  node *baru *bantu;
+baru = new node;
+baru->data = databaru;
+baru->next = baru;
+  if cek (head==1){
+    head=baru;
+    head->next=head;}
+  else {
+    help=head;
+    while (bantu->next!=head){
+      bantu=bantu->next;}
+    bantu->next=baru;
+    baru->next=head;
+	}  
+}
 
 int main(){
-
-
+	init();
 }
